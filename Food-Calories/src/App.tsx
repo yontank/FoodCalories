6aa30ab82dabbb99baed19af576d845af3154124
yontank <yontank007@gmail.com>
2025-02-10 14:20:30 +0200

    add react-query

--- Food-Calories/src/App.tsx
+++ Food-Calories/src/App.tsx
@@ -1,7 +1,6 @@
 import SearchBar from "./components/SearchBar";
 import NavBar from "./components/NavBar";
 import SearchItems from "./components/SearchItems";
-import { QueryClientProvider } from "@tanstack/react-query";
 function App() {
   return (
     <>
--- Food-Calories/src/main.tsx
+++ Food-Calories/src/main.tsx
@@ -1,15 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './input.css'
-import { BrowserRouter, Route, Routes } from 'react-router'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./input.css";
+import { BrowserRouter, Route, Routes } from "react-router";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 
-createRoot(document.getElementById('root')!).render(
+const queryClient = new QueryClient();
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
+    <QueryClientProvider client={queryClient}>
       <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<App />} />
-    </Routes>
-  </BrowserRouter>
-  </StrictMode>,
-)
+        <Routes>
+          <Route path="/" element={<App />} />
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
+  </StrictMode>
+);
