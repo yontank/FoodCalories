1f115619da4c8d2050717695472c040b8e130e42
yontank <yontank007@gmail.com>
2025-02-10 12:48:04 +0200

    change folder name to api

---
+++ backend/api/api.py
@@ -0,0 +1,17 @@
+from typing import List
+from fastapi import APIRouter, HTTPException
+from db import session
+from models import *
+from schemas import *
+from sqlalchemy import func, orm
+
+router = APIRouter()
+
+
+@router.get('/foods',response_model=List[ListFood])
+def list_food():
+    if foods := session.query(moh_mitzrachim).limit(5).all():
+        try:  
+            return map(lambda food: ListFood.model_validate(food) , foods)
+        except Exception as e:
+            print(e.errors())
