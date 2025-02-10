f3588fbfbb8bfd17aa72a2829aaa5c31a0e67397
yontank <yontank007@gmail.com>
2025-02-10 15:02:36 +0200

    wrap api on array, so we can add things later.

--- backend/api/api.py
+++ backend/api/api.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import Dict, List
 from fastapi import APIRouter, HTTPException
 from db import session
 from models import *
@@ -8,10 +8,10 @@ from sqlalchemy import func, orm
 router = APIRouter()
 
 
-@router.get('/foods',response_model=List[ListFood])
+@router.get('/foods',response_model=Dict[str, List[ListFood]])
 def list_food():
     if foods := session.query(moh_mitzrachim).limit(50).all():
         try:  
-            return map(lambda food: ListFood.model_validate(food) , foods)
+            return {'data': map(lambda food: ListFood.model_validate(food) , foods)}
         except Exception as e:
             print(e.errors())
