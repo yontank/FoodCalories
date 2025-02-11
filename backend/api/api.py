b5c22deac1c4ee838d4ad6eb1acec5c36a41090e
yontank <yontank007@gmail.com>
2025-02-11 14:18:25 +0200

    add ability to query foods

--- backend/api/api.py
+++ backend/api/api.py
@@ -10,8 +10,18 @@ router = APIRouter()
 
 @router.get('/foods',response_model=Dict[str, List[ListFood]])
 def list_food():
-    if foods := session.query(moh_mitzrachim).limit(50).all():
+    
+    if foods := session.query(moh_mitzrachim).limit(20).all():
+        
         try:  
             return {'data': map(lambda food: ListFood.model_validate(food) , foods)}
+        
         except Exception as e:
             print(e.errors())
+            
+
+@router.get('/foods/{food_query}')
+def query_foods(food_query : str):
+    if foods := session.query(moh_mitzrachim).filter(moh_mitzrachim.shmmitzrach.contains(food_query)).limit(5).all():
+        return {'data': foods}
+    return HTTPException(404, "food type wasnt found")
