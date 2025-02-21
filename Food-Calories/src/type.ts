ca808c174e2ab379006216967ca7bae89d1a4f4b
yontank <yontank007@gmail.com>
2025-02-21 23:19:52 +0200

    add types for fetching

--- Food-Calories/src/type.ts
+++ Food-Calories/src/type.ts
@@ -1,10 +1,43 @@
-interface ListFood {
+export interface ListFoodBase {
   code: number;
   shmmitzrach: string;
   smlmitzrach: number;
   english_name: string;
 }
 
+export interface ListFoodFull extends ListFoodBase {
+  // makor: number;
+  // edible: number;
+  // psolet: number;
+  // ahuz_ibud_nozlim: number;
+
+  protein: number;
+  total_fat: number;
+  carbohydrates: number;
+  food_energy: number;
+
+  midot: {
+    mishkal: number;
+    mida: number;
+    name: { smlmida: number; shmmida: string };
+  }[];
+
+  // alcohol: number;
+  // moisture: number;
+  // total_dietary_fiber: number;
+  // calcium: number;
+  // iron: number;
+  // magnesium: number;
+  // sodium: number;
+  // vitamin_a_iu: number;
+}
+
 export interface ListFoodAPI {
-  data: ListFood[];
+  data: ListFoodBase[];
+}
+
+export enum TIME {
+  BREAKFAST,
+  AFTERNOON,
+  EVENING,
 }
