5ad00f1ac547b3be59611f6ff33560fefe8f8bdd
yontank <yontank007@gmail.com>
2025-02-21 23:16:29 +0200

    get food data by food's name

--- backend/api/api.py
+++ backend/api/api.py
@@ -5,6 +5,7 @@ from models import *
 from schemas import *
 from sqlalchemy import func, orm
 
+
 router = APIRouter()
 
 
@@ -12,23 +13,24 @@ router = APIRouter()
 def list_food():
     
     if foods := session.query(moh_mitzrachim).limit(20).all():
-        
         try:  
             return {'data': map(lambda food: ListFood.model_validate(food) , foods)}
-        
         except Exception as e:
             print(e.errors())
+        raise HTTPException(403, 'Forbidden')
             
 
-@router.get('/foods/{food_query}', response_model=Dict[str, List[ListFood]])
+@router.get('/foods/{food_query}', response_model=Dict[str, List[QueryFood]])
 def query_foods(food_query : str):
     if foods := session.query(moh_mitzrachim).filter(moh_mitzrachim.shmmitzrach.contains(food_query)).limit(5).all():
         return {'data': foods}
     raise HTTPException(404, "food type wasnt found")
 
 
-@router.get('/food/{food_query}')
-def query_food(food_query: int):
-    if food := session.query(moh_mitzrachim).filter(moh_mitzrachim.code == food_query).first():
-        return food
+@router.get('/food/{food_query}', response_model=Dict[str, QueryFood])
+def query_food(food_query: str):
+    
+    if food := session.query(moh_mitzrachim).filter(moh_mitzrachim.shmmitzrach.__eq__(food_query)).first():
+        return {'data': food}
     raise HTTPException(404, detail='item not found')
+
