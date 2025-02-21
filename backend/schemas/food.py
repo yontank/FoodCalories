8eb9891691edd51859ff6499f3b4ac7001748437
yontank <yontank007@gmail.com>
2025-02-21 23:15:31 +0200

    add models for midot

--- backend/schemas/food.py
+++ backend/schemas/food.py
@@ -1,18 +1,42 @@
-from typing import Optional
+from typing import Optional, List
 from pydantic import BaseModel, Field, ConfigDict
-
+from pydantic import (PositiveInt)
 
 
 class FoodBase(BaseModel):
-    code: int 
-    smlmitzrach : int
-    shmmitzrach : str
+    code: int
+    smlmitzrach: int
+    shmmitzrach: str
+    
+class YehidaBase(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+    smlmida: int
+    shmmida: Optional[str]
     
     
+    
+class YehidotMidaMitzrachim(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+    mida: int
+    mishkal: int
+    name: YehidaBase
+
+
+
 class ListFood(FoodBase):
     model_config = ConfigDict(from_attributes=True)
-    english_name : Optional[str] = None
+    english_name: Optional[str] = None
+
+
+class QueryFood(FoodBase):
+    model_config = ConfigDict(from_attributes=True)
     
+    protein: Optional[float] = None
+    total_fat: Optional[float] = None
+    carbohydrates: Optional[float] = None
+    food_energy :Optional[PositiveInt]
+    
+    midot: List[YehidotMidaMitzrachim]
     
     
     
