31a61cb8a79da865446b3ff4f96c40c4144e4277
Yehonatan Avrahimi <48332126+yontank@users.noreply.github.com>
2026-02-04 18:02:37 +0200

    add readme

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,9 @@
 import uvicorn
+import logging
 
 
 if __name__ == '__main__':
+    logging.basicConfig(level=logging.DEBUG)
+    logging.debug('Starting main.py')
+    logging.info('Launching Uvicorn server for api:app...')
     uvicorn.run('api:app', host='0.0.0.0', port=8000, reload=True)
