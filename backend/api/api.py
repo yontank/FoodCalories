3442a73623c9f4f081dd9fa7a93f520234fe3505
Yehonatan Avrahimi <48332126+yontank@users.noreply.github.com>
2026-02-18 13:58:58 +0200

    feat: add endpoint to retrieve meals by date range and refactor food query parameter

--- backend/api/api.py
+++ backend/api/api.py
@@ -1,6 +1,7 @@
+from datetime import datetime, timedelta
 from typing import Annotated
 from fastapi import APIRouter, Depends, HTTPException
-from sqlalchemy import select, update
+from sqlalchemy import select
 from fastapi import status
 
 
@@ -15,12 +16,9 @@ from ..db import session
 
 router = APIRouter()
 
-# FIXME: Don't use / , use a query param.
-# example. q={QUERY}
 
-
-@router.get('/foods/{food_query}', response_model_by_alias=False, response_model=dict[str, list[FoodDetail]])
-def query_foods(food_query: str, current_user: Annotated[JWTAccessBase, Depends(get_current_user)]):
+@router.get('/foods', response_model_by_alias=False, response_model=dict[str, list[FoodDetail]])
+def query_foods(food_query: str, _: Annotated[JWTAccessBase, Depends(get_current_user)]):
     if foods := session.query(MohMitzrachim).filter(MohMitzrachim.shmmitzrach.contains(food_query)).limit(20).all():
         return {'data': foods}
     raise HTTPException(404, "food type wasnt found")
@@ -88,3 +86,36 @@ def update_meal(
         setattr(db_meal, k, v)
 
     session.commit()
+
+
+@router.get(f'/meals', response_model_by_alias=False,  status_code=status.HTTP_200_OK)
+def get_meals_by_date_start_end_date(
+        current_user: Annotated[JWTAccessBase, Depends(get_current_user)],
+        date: datetime,
+        end_date: datetime | None = None):
+    """
+    Returns all the meals the user has consumed in a specific date, or between a range of dates.
+    """
+
+    if end_date and end_date < date:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
+                            detail="End date must be after start date")
+
+    start_of_start_date = date.replace(
+        minute=0, second=0, microsecond=0, hour=0)
+
+    if not end_date:
+        end_of_end_date = start_of_start_date + timedelta(days=1)
+    else:
+        end_of_end_date = end_date.replace(
+            minute=0, second=0, microsecond=0, hour=0) + timedelta(days=1)
+
+    stmt = (
+        select(MealsEaten).where(MealsEaten.user_id == current_user.sub,
+                                 MealsEaten.date >= start_of_start_date,
+                                 MealsEaten.date < end_of_end_date)
+    )
+
+    meals = session.execute(stmt).scalars().all()
+
+    return {"data": meals}
