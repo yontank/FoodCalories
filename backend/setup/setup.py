6ebcafa0122ec8946c8d35ae252d08d8b75e6810
yontank <yontank007@gmail.com>
2026-02-26 14:41:50 +0200

    fix type errors in setup.py

--- backend/setup/setup.py
+++ backend/setup/setup.py
@@ -1,51 +1,51 @@
-from backend.setup.models import MohYehidotMidaLemitzrachim
+"""A Setup module for when you want to create a database to make everything u use to work"""
 
+import csv
+from pathlib import Path
+from typing import TypeVar
 
 from pydantic.main import BaseModel
 from sqlalchemy import create_engine
-from sqlalchemy.orm import DeclarativeBase
-from pathlib import Path
-# pyright: ignore[reportAttributeAccessIssue]
+from sqlalchemy.orm import DeclarativeBase, Session
 from sqlalchemy_utils import create_database, database_exists
-from sqlalchemy.orm import Session
 
+from ..schemas.based import Base
 from ..schemas.moh_mitzrachim import MohMitzrachim as MohMitzrachimDB
 from ..schemas.moh_yehidot_mida import YehidotMida as YehidotMidaDB
-from ..schemas.moh_yehidot_mida_lemitzrachim import YehidotMidaLemitzrachim as YehidotMidaLemitzrachimDB
-from ..schemas.based import Base
-
-
+from ..schemas.moh_yehidot_mida_lemitzrachim import (
+    YehidotMidaLemitzrachim as YehidotMidaLemitzrachimDB,
+)
 from .models import MohMitzrachim, MohYehidotMida, MohYehidotMidaLemitzrachim
-import csv
+
+T = TypeVar("T", bound=BaseModel)
+S = TypeVar("S", bound=DeclarativeBase)
 
 
-def read_csv_file(f_csv: Path, ValidationModel: type[BaseModel]) -> list[type[BaseModel]]:
-    res: list[type[BaseModel]] = []
-    with open(f_csv, encoding='utf-8-sig') as csv_file:
+def read_csv_file(f_csv: Path, ValidationModel: type[T]) -> list[T]:
+    """Given a CSV file from israeli-foods, check them via pydantic models to make sure they are file, if they are, return list of pydantic instances."""
+    res: list[T] = []
+    with open(f_csv, encoding="utf-8-sig") as csv_file:
         reader = csv.DictReader(csv_file)
         for row in reader:
 
             for k, v in row.items():
-                if v == '':
+                if v == "":
                     row[k] = None
 
-            model: type[BaseModel] = ValidationModel.model_validate(  # pyright: ignore[reportAssignmentType]
-                row, from_attributes=True)
+            model: T = ValidationModel.model_validate(row, from_attributes=True)
 
             res.append(model)
     return res
 
 
-def insert_to_db(schema: type[DeclarativeBase], models: list[type[BaseModel]], session: Session):
-    # session.add_all([schema(*model) for model in models])
+def insert_to_db(schema: type[S], models: list[T], session: Session):
     session.add_all([schema(**model.model_dump()) for model in models])
     session.commit()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # If the Database, exists, raise an error because setup.py should get an non-existing database.
-    engine = create_engine(
-        "postgresql://postgres:postgres@localhost:5432/ARE")
+    engine = create_engine("postgresql://postgres:postgres@localhost:5432/ARE")
 
     if not database_exists(engine.url):
         create_database(engine.url)
@@ -55,20 +55,22 @@ if __name__ == '__main__':
 
     moh_mitzrachim_file = Path("CSV/moh_mitzrachim.csv")
     moh_yehidot_mida_file = Path("CSV/moh_yehidot_mida.csv")
-    moh_yehidot_mida_lemitzrachim_file = Path(
-        "CSV/moh_yehidot_mida_lemitzrachim.csv")
+    moh_yehidot_mida_lemitzrachim_file = Path("CSV/moh_yehidot_mida_lemitzrachim.csv")
 
-    mitz: list[MohMitzrachim] = read_csv_file(  # pyright: ignore[reportAssignmentType]
-        moh_mitzrachim_file, MohMitzrachim)
+    mitz: list[MohMitzrachim] = read_csv_file(moh_mitzrachim_file, MohMitzrachim)
     yeh: list[MohYehidotMida] = read_csv_file(
-        # pyright: ignore[reportAssignmentType]
-        moh_yehidot_mida_file, MohYehidotMida)
-    yehmle: list[MohYehidotMidaLemitzrachim] = read_csv_file(moh_yehidot_mida_lemitzrachim_file,  # pyright: ignore[reportAssignmentType]
-                                                             MohYehidotMidaLemitzrachim)
+        moh_yehidot_mida_file,
+        MohYehidotMida,
+    )
+    yehmle: list[MohYehidotMidaLemitzrachim] = read_csv_file(
+        moh_yehidot_mida_lemitzrachim_file,
+        MohYehidotMidaLemitzrachim,
+    )
 
     PK_mitz = {x.code for x in mitz}
     PK_yeh = {x.smlmida for x in yeh}
 
+    # NOTE: for some reason, MohYehidotMidaLemitzrachim is a many-to-many table that contains foreign keys that doesnt exist in the other tables, so we decided to filter them out.
     yml: list[MohYehidotMidaLemitzrachim] = [
         x for x in yehmle if x.mida in PK_yeh and x.mmitzrach in PK_mitz
     ]
