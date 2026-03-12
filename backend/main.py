77a6f6bdd423caefec7bbf1c862293bec4038c65
Ron Tseytlin <ronts390@gmail.com>
2026-03-12 17:18:08 +0200

    build frontend into backend and serve it from backend

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,11 @@
+from fastapi.staticfiles import StaticFiles
 import uvicorn
 
 from slowapi import _rate_limit_exceeded_handler
 from slowapi.errors import RateLimitExceeded
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from starlette.exceptions import HTTPException
 
 from core.config import settings
 from core.rate_limit import limiter
@@ -22,6 +24,22 @@ app.add_middleware(
     allow_headers=["*"],
 )
 app.include_router(router=router.router, prefix='/api')
+
+# Define a subclass of StaticFiles to create a custom static file handler
+# that supports client-side routing in Single Page Applications (SPAs).
+class SPAStaticFiles(StaticFiles):
+    # Override the get_response method, which is responsible for retrieving
+    # static file responses for given paths.
+    async def get_response(self, path: str, scope):
+        try:
+            return await super().get_response(path, scope)
+        except HTTPException as ex:
+            if ex.status_code == 404:
+                return await super().get_response("index.html", scope)
+            else:
+                raise ex
+
+app.mount("/", SPAStaticFiles(directory="frontend", html=True), name="spa")
 
 def main():
     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
