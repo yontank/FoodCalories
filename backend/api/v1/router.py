ae723149a8cfd8492af448305d11a7840f1dc1c4
yontank <yontank007@gmail.com>
2026-03-15 13:07:37 +0200

    add health endpoint

--- backend/api/v1/router.py
+++ backend/api/v1/router.py
@@ -1,10 +1,12 @@
-from fastapi import Request, APIRouter
+from datetime import datetime
+
+from fastapi import Request, APIRouter, status
+from fastapi.responses import JSONResponse
 from core.rate_limit import limiter
 from api.v1 import meal_api, login
 
 
-router = APIRouter(prefix='/v1')
-
+router = APIRouter(prefix="/v1")
 
 
 router.include_router(meal_api.router)
@@ -15,3 +17,8 @@ router.include_router(login.router)
 @limiter.limit("1/minute")
 def test(request: Request):
     return "monkey"
+
+
+@router.get("/health", status_code=status.HTTP_200_OK)
+async def health_check():
+    return JSONResponse(content={"status": "healthy", "timestamp": datetime.now()})
