91b51ac225809b3f443dea20ed93d9141e2453cd
yontank <yontank007@gmail.com>
2026-03-18 19:38:43 +0200

    increase bottom padding of meal entries for bigger UI eperience

--- frontend/src/components/MealsEatenContainer.tsx
+++ frontend/src/components/MealsEatenContainer.tsx
@@ -18,7 +18,7 @@ export function MealsEatenContainer({
   const eatenToday = eatenFood?.map((e) => (
     <div
       key={e.meal_id}
-      className="rounded-lg border border-border bg-card text-card-foreground shadow-sm h-auto w-full text-center flex p-3"
+      className="rounded-lg border border-border bg-card text-card-foreground shadow-sm h-auto w-full text-center flex p-3 "
     >
       <div className="flex flex-col w-1/3 max-w-3xl text-right">
         <b>{e.food_name}</b>
@@ -51,7 +51,7 @@ export function MealsEatenContainer({
   ));
 
   return (
-    <div className="rounded-xl border-4 p-2">
+    <div className="rounded-xl border-4 p-2 min-h-48">
       <div className="flex items-center gap-3 mb-2">
         <Button
           className="rounded-full w-12 h-12"
