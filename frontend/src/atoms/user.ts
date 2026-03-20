ec7467d21d11fcabe5d407ff8eaed4a3810cf6aa
Ron Tseytlin <ronts390@gmail.com>
2026-03-21 01:09:19 +0200

    fixed access token storage (wtf??)

--- frontend/src/atoms/user.ts
+++ frontend/src/atoms/user.ts
@@ -1,8 +1,7 @@
 import { atomWithStorage } from "jotai/utils";
-import i18next from 'i18next'
 
 export const accessTokenAtom = atomWithStorage<string | undefined>(
-  i18next.t('accesstoken', 'accessToken'),
+  "accessToken",
   undefined,
   undefined,
   { getOnInit: true },
